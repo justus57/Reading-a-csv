59889510fe07b5b234ee5c181fe97bd057388122
Joseph K Justus <justuskasyoki57@gmail.com>
2022-09-10 19:46:43 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using CsvHelper;
+﻿
+using CsvHelper;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,35 +13,71 @@ namespace csv
     {
         static void Main(string[] args)
         {
-            var data = new[]
-        {
-                new Project { CustomerName = "Big Corp", Title = "CRM updates", Deadline = DateTime.Today.AddDays(-2) },
-                new Project { CustomerName = "Imaginary Corp", Title = "Sales system", Deadline = DateTime.Today.AddDays(1) }
-            };
-
-            using (var mem = new MemoryStream())
-            using (var writer = new StreamWriter(mem))
-            using (var csvWriter = new CsvWriter(writer))
+            // Write sample data to CSV file
+            using (CsvFileWriter writer = new CsvFileWriter("WriteTest.csv"))
             {
-                csvWriter.Configuration.Delimiter = ";";
-
-                csvWriter.WriteField("Customer");
-                csvWriter.WriteField("Title");
-                csvWriter.WriteField("Deadline");
-                csvWriter.NextRecord();
-
-                foreach (var project in data)
+                for (int i = 0; i < 100; i++)
                 {
-                    csvWriter.WriteField(project.CustomerName);
-                    csvWriter.WriteField(project.Title);
-                    csvWriter.WriteField(project.Deadline);
-                    csvWriter.NextRecord();
+                    CsvRow row = new CsvRow();
+                    for (int j = 0; j < 5; j++)
+                        row.Add(String.Format("Column{0}", j));
+                    writer.WriteRow(row);
                 }
-
-                writer.Flush();
-                var result = Encoding.UTF8.GetString(mem.ToArray());
-                Console.WriteLine(result);
             }
         }
     }
+    /// </summary>
+    public class CsvRow : List<string>
+    {
+        public string LineText { get; set; }
+    }
+
+    /// <summary>
+    /// Class to write data to a CSV file
+    /// </summary>
+    public class CsvFileWriter : StreamWriter
+        {
+            public CsvFileWriter(Stream stream)
+                : base(stream)
+            {
+            }
+
+            public CsvFileWriter(string filename)
+                : base(filename)
+            {
+            }
+
+            /// <summary>
+            /// Writes a single row to a CSV file.
+            /// </summary>
+            /// <param name="row">The row to be written</param>
+            public void WriteRow(CsvRow row)
+            {
+                StringBuilder builder = new StringBuilder();
+                bool firstColumn = true;
+                foreach (string value in row)
+                {
+                    // Add separator if this isn't the first value
+                    if (!firstColumn)
+                        builder.Append(',');
+                    // Implement special handling for values that contain comma or quote
+                    // Enclose in quotes and double up any double quotes
+                    if (value.IndexOfAny(new char[] { '"', ',' }) != -1)
+                        builder.AppendFormat("\"{0}\"", value.Replace("\"", "\"\""));
+                    else
+                        builder.Append(value);
+                    firstColumn = false;
+                }
+                row.LineText = builder.ToString();
+                WriteLine(row.LineText);
+            }
+        }
+    
+    public class Project
+    {
+        public string EmployeeID;
+        public string Managerid;
+        public string Salary;
+    }
 }
+
